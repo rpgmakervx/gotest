089b93ddb298cd7971f9c1d0d188ba37f50b2f35
xingtianyu03 <xingtianyu03@kuaishou.com>
2019-12-10 00:45:39 +0800

    test day 4

--- src/teststruct/StructTest.go
+++ src/teststruct/StructTest.go
@@ -3,6 +3,7 @@ package teststruct
 import (
 	"fmt"
 	"time"
+	"unsafe"
 )
 
 type JobInfo struct {
@@ -10,10 +11,15 @@ type JobInfo struct {
 	jobType int
 	time    int64
 }
+type Object struct {
+	name string
+}
 
 func TestStruct() {
 	testDefine()
 	testVisit()
+	testEquals()
+	testSize()
 }
 
 func testDefine() {
@@ -40,4 +46,22 @@ func testVisit() {
 	job.id = "job-2"
 	job.jobType = 2
 	fmt.Println("job is:", job)
+}
+
+func testEquals() {
+	var job1 = JobInfo{"job-1", 1, 1}
+	var job2 = JobInfo{"job-1", 1, 1}
+	var job3 = JobInfo{"job-3", 3, 1}
+	fmt.Printf("job1 addr:%p job2 addr2:%p, job3 addr:%p\n", &job1, &job2, &job3)
+	fmt.Println("job1 == job2? :", job1 == job2)
+	fmt.Println("job1 == job3? :", job1 == job3)
+	fmt.Println("job1 addr == job2 addr ? :", &job1 == &job3)
+}
+
+func testSize() {
+	var object = Object{name: "12345678901234567890"}
+	var p uintptr
+	fmt.Printf("object len:%d\n", unsafe.Sizeof(object))
+	fmt.Printf("string len:%d\n", unsafe.Sizeof(""))
+	fmt.Printf("uintptr len:%d\n", unsafe.Sizeof(p))
 }
